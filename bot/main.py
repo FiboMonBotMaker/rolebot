39d4c9b31a91dc9cecc51e3a8bf635a18fe6d109
nikawamikan <wkwktktkkrkr@outlook.jp>
2022-08-23 22:31:05 +0900

    基本機能完成

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,20 @@ from discord.ext import commands
 import os
 import pathlib
 import discord
+from lib.dbutil import create_talbe, primary_key
+
+
+# DataBaseの初期化 既にテーブルが存在している場合は作成しない
+create_talbe(
+    table_name="roles",
+    columns=[
+        "guild_id bigint",
+        "role_id bigint",
+        "INDEX guids_index(guild_id)",
+        primary_key(["guild_id", "role_id"])
+    ]
+)
+
 
 intent = discord.Intents.all()
 bot = commands.Bot(debug_guilds=[os.getenv(
@@ -19,7 +33,7 @@ async def on_ready():
 @bot.event
 async def on_application_command_error(ctx: discord.ApplicationContext, error: Exception):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.respond(error, ephemeral=True)
+        await ctx.respond("This command is for administrators only !!", ephemeral=True)
     else:
         raise error
 
